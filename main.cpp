0f9556bf455310cc518e1b63608a7d61c43cd0b2
H0peuless <hopeuleuss@gmail.com>
2024-09-30 11:43:28 -0600

    Implementation of the first function and the parsing function

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include "pixel.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+Pixel make_pixel(std::string line){
+  Pixel pixel;
+  pixel.x = stoi(line.substr(0, line.find(",")));
+  pixel.y = stoi(line.substr(1, line.find(",")));
+  pixel.r = stof(line.substr(2, line.find(",")));
+  pixel.g = stof(line.substr(3, line.find(",")));;
+  pixel.b = stof(line.substr(4, line.find(",")));;
+
+  return pixel;
+}
+
+void average_colors(std::vector<Pixel> &pixel_list){
+  float r = 0;
+  float g = 0;
+  float b = 0;
+
+  for (int i = 0; i < pixel_list.size(); i++){
+    r += pixel_list[i].r;
+    g += pixel_list[i].g;
+    b += pixel_list[i].b;
+  }
+  r /= pixel_list.size();
+  g /= pixel_list.size();
+  b /= pixel_list.size();
+  std::cout << "Average value r: " << r << " g: " << g << " b: " << b << std::endl;
+};
+
+
+
+int main(const int argc, const char * argv[]) {
+  std::vector<Pixel> pixel_list;
+  std::ifstream file;
+  std::string line;
+
+  if (argc > 1 && std::stoi(argv[1])){ //choose the file source
+    std::ifstream file(argv[1]);
+  }else{
+    std::ifstream file("pixels.dat");
+  }
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      std::cout << line << std::endl; //Read line by line
+      pixel_list.push_back(make_pixel(line)); //Parse the line to a pixel Object
+    }
+    file.close();
+
+    average_colors(pixel_list);
+
+
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+
+
+  return 0;
+}
--- pixel.h
+++ pixel.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 struct _pixel
 {
   float r;
